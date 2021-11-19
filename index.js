80cf4f6f1e5404d129a128b212f4241aa15769f2
Yen <yen.hnguyen17@gmail.com>
2021-11-19 05:19:45 +0000

    Add a function to fetch ISS flyovers

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+// const { fetchCoordsByIP } = require('./iss');
+// const { fetchISSFlyOverTimes } = require(./iss)
+ 
 
 fetchMyIP((error, ip) => {
   if (error) return console.log("It didn't work!", error);
@@ -11,4 +13,10 @@ fetchCoordsByIP("149.248.186.71", (error, data) => {
   if (error) return console.log("It didn't work!", error);
 
   console.log("It worked! Returned IP: ", data);
+});
+
+fetchISSFlyOverTimes("149.248.186.71", (error, data) => {
+  if (error) return console.log("It didn't work!", error);
+
+  console.log(data);
 });
--- iss.js
+++ iss.js
@@ -47,4 +47,33 @@ const fetchCoordsByIP = function(ip, callback) {
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+const fetchISSFlyOverTimes = function(coords, callback) {
+  // let coords = fetchCoordsByIP;
+  const url = `https://iss-pass.herokuapp.com/json/?lat=43.6426&lon=-79.4002`;
+
+  request(url, (error, response, body) => {
+    if (error) return callback(error);
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching ISS flyovers. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    callback(null, data.response);
+  });
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
